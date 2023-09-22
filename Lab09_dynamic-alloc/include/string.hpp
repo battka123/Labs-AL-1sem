f43c41900a442dbdf38064d6c7e8f875dfb54354
Ivan <55855887+battka123@users.noreply.github.com>
2023-09-22 15:26:07 +0300

    Add files via upload

---
+++ Lab09_dynamic-alloc/include/string.hpp
@@ -0,0 +1,10 @@
+// Copyright 2022 Ivan Sushin <i.suschin@yamdex.ru>
+#ifndef INCLUDE_STRING_HPP_
+#define INCLUDE_STRING_HPP_
+
+#include <cstring>
+
+char* CopyString(const char* str);
+char* ConcatinateStrings(const char* a, const char* b);
+
+#endif  // INCLUDE_STRING_HPP_
